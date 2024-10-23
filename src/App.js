a30435842a04e55f7a3d1aca4f1e779ea9d6c016
ReynaPatel <patelreyna2323@gmail.com>
2024-10-23 14:32:14 -0500

    small bug fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import InteractiveSolarSystem from './InteractiveSolarSystem';
+import InteractiveSolarSystem from './InteractiveSolarSystem.js';
 
 const App = () => {
   return (
