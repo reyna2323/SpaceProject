5e3f693a711335ac0484e7e694ed0b669cd9da54
ReynaPatel <patelreyna2323@gmail.com>
2024-10-25 09:44:10 -0500

    initial code for adding planets onto solar system (does not work yet)

--- src/InteractiveSolarSystem.js
+++ src/InteractiveSolarSystem.js
@@ -1,19 +1,21 @@
 // InteractiveSolarSystem.js
-import React from 'react';
-import SolarSystem from './SolarSystem';   // Import the solar system simulation
-import PhysicsInfo from './PhysicsInfo';   // Import the physics info
-import './InteractiveSolarSystem.css';     // Import styles
+import React, { useState } from 'react';
+import SolarSystem from './SolarSystem';
+import PhysicsInfo from './PhysicsInfo';
+import './InteractiveSolarSystem.css';
 
 const InteractiveSolarSystem = () => {
+  const [physicsData, setPhysicsData] = useState({ force: 0, distance: 0, velocity: { x: 0, y: 0 } });
+
   return (
     <div className="interactive-solar-system">
       <h1>Explore the Physics of Orbits</h1>
       <div className="content">
         <div className="simulation">
-          <SolarSystem />
+          <SolarSystem onPhysicsDataUpdate={setPhysicsData} />
         </div>
         <div className="info">
-          <PhysicsInfo />
+          <PhysicsInfo data={physicsData} />
         </div>
       </div>
     </div>
